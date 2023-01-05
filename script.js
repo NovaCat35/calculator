0fc6d25a647b700adc792379c7e1d2f9a98c3142
Robert Chen <bergintonatcapital@gmail.com>
2023-01-05 01:40:36 -0500

    Add clear, decimal, back, and operation functions

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-let displayResult = document.querySelector('.resultDisplay');
-let buttons = document.querySelectorAll('button');
+const displayResult = document.querySelector('.resultDisplay');
+const buttons = document.querySelectorAll('button');
 
-let number = '';
-buttons.forEach(button => button.addEventListener('mousedown', function(event){
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result = 0;
+
+buttons.forEach(button => button.addEventListener('click', function (event) {
     let btnValue = event.target.innerText;
     let btnClass = event.target.classList.value;
-    console.log(btnClass)
-    // if number display
-    // if operator, wait for 2nd number
-    // call operate() when classbtnValue is equal or another operator shows up after number
-    // 
+
     if(btnClass == 'clearBtn') {
         clear();
     }
-    if(btnClass == 'divBtn' || btnClass == 'mulBtn' || btnValue == 'subBtn' || btnValue == 'addBtn') {
-        
+    else if(btnClass == 'backBtn') {
+        back();
     }
-    if(btnClass == 'numBtn') {
-        number += btnValue;
-        displayResult.innerText = number;
+    else if(btnClass == 'dotBtn') {
+        decimalInput();
+    }
+    else if(btnClass == 'divBtn' || btnClass == 'mulBtn' || btnClass == 'subBtn' || btnClass == 'addBtn') {
+        operator = btnValue;
+    }
+    else if(btnClass == 'numBtn') {
+        // Case 1: Starting off 1st operand, we have not hit operator yet
+        console.log(operator)
+        if(operator == '') {
+            firstNum += btnValue;
+            displayResult.innerText = firstNum;
+        // Case 2: We have enter operator and ready for 2nd operand
+        } else if(operator != '') {
+            secondNum += btnValue;
+            displayResult.innerText = secondNum;
+        }
     }
 }))
 
@@ -35,30 +49,49 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if (num2 == 0) {
-        alert(`ERROR... \n I'm afraid I can't do that.`) 
+    if(num2 == 0) {
+        alert(`ERROR... \n I'm afraid I can't do that.`)
     }
     return num1 /= num2;
 }
 
 function clear() {
-
+    firstNum = '';
+    secondNum = '';
+    displayResult.innerText = 0;
 }
 
-function operate(num1, num2, operator) {
+function back() {
+    firstNum = firstNum.slice(0, -1);
+    displayResult.innerText = firstNum;
+}
+
+function decimalInput() {
+    // Case 1: we start with decimal
+    if (firstNum == '') {
+        firstNum = '0.'
+    } 
+    // Case 2: not the first number and does not already contain decimal
+    else if(!firstNum.includes('.')) {
+        firstNum += '.'
+    } 
+    displayResult.innerText = firstNum;
+}
+
+function evaluate(num1, num2, operator) {
     let result = 0;
     switch (operator) {
         case 'add':
-            result = add(num1,num2);
+            result = add(num1, num2);
             break;
         case 'subtract':
-            result = subtract(num1,num2);
-            break;  
+            result = subtract(num1, num2);
+            break;
         case 'multiply':
-            result = multiply(num1,num2);
-            break;          
+            result = multiply(num1, num2);
+            break;
         case 'divide':
-            result = divide(num1,num2);
+            result = divide(num1, num2);
             break;
         default:
             console.log('error')
