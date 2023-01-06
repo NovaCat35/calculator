892e157533874494b7a051c5b857f9aba86c7a18
Robert Chen <bergintonatcapital@gmail.com>
2023-01-06 01:25:32 -0500

    Implement evaluation on calculation and clearBtn, backBtn, decimal"

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttons = document.querySelectorAll('button');
 let firstNum = '';
 let secondNum = '';
 let operator = '';
-let result = 0;
+let result = '';
 
 buttons.forEach(button => button.addEventListener('click', function (event) {
     let btnValue = event.target.innerText;
@@ -14,87 +14,127 @@ buttons.forEach(button => button.addEventListener('click', function (event) {
         clear();
     }
     else if(btnClass == 'backBtn') {
-        back();
-    }
-    else if(btnClass == 'dotBtn') {
-        decimalInput();
+        // Case 1: we're working with firstNum so we haven't made operator yet
+        if(operator == '') {
+            back('1stOperand');
+        } else {
+            back('2ndOperand');
+        }
     }
     else if(btnClass == 'divBtn' || btnClass == 'mulBtn' || btnClass == 'subBtn' || btnClass == 'addBtn') {
+        // We have already an given num1, num2, and operator. Go ahead and evaluate result.
+        // Then, we assign firstNum as result to be use to calculate next expression
+        if(secondNum != '') {
+            result = evaluate(firstNum, secondNum, operator);
+            displayResult.innerText = result;
+            firstNum = result;
+            secondNum = '';
+            result = '';
+        }
         operator = btnValue;
     }
     else if(btnClass == 'numBtn') {
         // Case 1: Starting off 1st operand, we have not hit operator yet
-        console.log(operator)
         if(operator == '') {
             firstNum += btnValue;
             displayResult.innerText = firstNum;
-        // Case 2: We have enter operator and ready for 2nd operand
-        } else if(operator != '') {
+        // Case 2: We have enter operator and ready for 2nd operand. Awaiting result
+        } else if(operator != '' && result == '') {
             secondNum += btnValue;
             displayResult.innerText = secondNum;
+        } 
+    }
+    else if(btnClass == 'equalBtn') {
+        if (firstNum == '' || secondNum == '') return;
+        result = evaluate(firstNum, secondNum, operator);
+        displayResult.innerText = result;
+    }
+    else if(btnClass == 'dotBtn') {
+        if(operator == '') {
+            decimalInput('1stOperand');
+        } else {
+            decimalInput('2ndOperand');
         }
+
     }
 }))
 
 function add(num1, num2) {
-    return num1 += num2;
+    return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 -= num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 *= num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
     if(num2 == 0) {
         alert(`ERROR... \n I'm afraid I can't do that.`)
     }
-    return num1 /= num2;
+    return num1 / num2;
 }
 
 function clear() {
     firstNum = '';
     secondNum = '';
+    operator = '';
+    result = '';
     displayResult.innerText = 0;
 }
 
-function back() {
-    firstNum = firstNum.slice(0, -1);
-    displayResult.innerText = firstNum;
+function back(operand) {
+    if(operand == '1stOperand') {
+        firstNum = firstNum.slice(0, -1);
+        displayResult.innerText = firstNum;
+    } else {
+        secondNum = secondNum.slice(0, -1);
+        displayResult.innerText = secondNum;
+    }
 }
 
-function decimalInput() {
-    // Case 1: we start with decimal
-    if (firstNum == '') {
-        firstNum = '0.'
-    } 
-    // Case 2: not the first number and does not already contain decimal
-    else if(!firstNum.includes('.')) {
-        firstNum += '.'
-    } 
-    displayResult.innerText = firstNum;
+function decimalInput(operand) {
+    switch (operand) {
+        case '1stOperand':
+            if (firstNum == '') {
+                firstNum = '0.';
+            } else if(!firstNum.includes('.')) {
+                firstNum += '.';
+            }
+            displayResult.innerText = firstNum;
+            break;
+        case '2ndOperand':
+            if (secondNum == '') {
+                secondNum = '0.';
+            } else if(!secondNum.includes('.')) {
+                secondNum += '.';
+            } 
+            displayResult.innerText = secondNum;
+            break;
+    }
 }
 
 function evaluate(num1, num2, operator) {
-    let result = 0;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     switch (operator) {
-        case 'add':
+        case '+':
             result = add(num1, num2);
             break;
-        case 'subtract':
+        case '-':
             result = subtract(num1, num2);
             break;
-        case 'multiply':
+        case '*':
             result = multiply(num1, num2);
             break;
-        case 'divide':
+        case '/':
             result = divide(num1, num2);
             break;
         default:
-            console.log('error')
+            console.log('error');
     }
-    return result;
+    return Math.round(result*100)/100;
 }
